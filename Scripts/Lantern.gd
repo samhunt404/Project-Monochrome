a0e67a91ccdd94003b3c1655ec86ad66b9062afc
samhunt404 <samuelhunt404@gmail.com>
2023-11-13 21:18:23 -0700

    Updated stuff, made film indicators

--- Scripts/Lantern.gd
+++ Scripts/Lantern.gd
@@ -1,5 +1,9 @@
 extends FilmManipulator
 
+var dim := preload("res://Materials/Bulb_dim.tres")
+var lit := preload("res://Materials/Bulb_lit.tres")
+
+@onready var bulb = $MagicLantern/Bulb
 @onready var spotLight := $SpotLight3D
 @onready var player = get_tree().get_nodes_in_group("Player")[0]
 
@@ -9,6 +13,8 @@ func _ready():
 
 func _sync_texture():
 	if hasFilm:
+		bulb.set_surface_override_material(1,lit)
 		spotLight.light_projector = film.tex
 	else:
+		bulb.set_surface_override_material(1,dim)
 		spotLight.light_projector = null
--- Scripts/camera_obscura.gd
+++ Scripts/camera_obscura.gd
@@ -5,18 +5,28 @@ class_name CameraObscura
 var texture : ViewportTexture
 var currentTexture
 
+var dim := preload("res://Materials/Bulb_dim.tres")
+var lit := preload("res://Materials/Bulb_lit.tres")
 
 @export var startsWithFilm := true
+
 @onready var player = get_tree().get_nodes_in_group("Player")[0]
 @onready var viewport := $SubViewport
 @onready var viewFinder = $ViewFinder
+@onready var bulb := $CameraObscura/Bulb
 
 func _ready():
 	film = Photo.new()
 	if startsWithFilm:
 		hasFilm = true
 		film.holder = self
+		bulb.set_surface_override_material(1,lit)
+	else:
+		bulb.set_surface_override_material(1,dim)
 		
+	given.connect(_updateFilmIndicator)
+	taken.connect(_updateFilmIndicator)
+
 	await RenderingServer.frame_post_draw
 	texture = viewport.get_texture()
 	#set viewfinder from script so godot doesn't break
@@ -29,3 +39,6 @@ func _take_picture():
 	if(hasFilm):
 		film.tex = ImageTexture.create_from_image(texture.get_image())
 
+func _updateFilmIndicator():
+	var curMat := lit if hasFilm else dim
+	bulb.set_surface_override_material(1,curMat)
